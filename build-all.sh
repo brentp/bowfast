23b022dc82647af1873bb72dc1a6c71daff0b82f
Brent Pedersen <bpederse@gmail.com>
2011-12-01 09:52:18 -0700

    update bowfast

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,14 @@
+git submodule update
+
 cd lib/sickle && make && cd -
 cd lib/samtools && make && cd -
 cd lib/bfast && ln -sf ../samtools . && sh autogen.sh && ./configure \
-    && make
+    && make && cd -
+
+if [  -x lib/bowtie-0.12.7/bowtie ]; then
+    cd lib
+    rm -rf bowtie*
+    wget http://downloads.sourceforge.net/project/bowtie-bio/bowtie/0.12.7/bowtie-0.12.7-src.zip
+    unzip bowtie-0.12.7-src.zip
+    cd bowtie-0.12.7 && make
+fi
