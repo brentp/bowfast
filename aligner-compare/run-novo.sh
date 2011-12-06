85f8bb3865141ae7b06ddce3961cbebb3a7539fc
Brent Pedersen <bpederse@gmail.com>
2011-12-06 09:15:23 -0700

    use 24 cpus.

--- aligner-compare/run-novo.sh
+++ aligner-compare/run-novo.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
-#BSUB -J novo.map[1-13]
+#BSUB -J novo.map
 #BSUB -e logs/novo.%I.%J.err
 #BSUB -o logs/novo.%I.%J.out
 #BSUB -R "rusage[mem=25800]"
-#BSUB -n 4
+#BSUB -n 12
 
 
-LC_ALL=C
-set -eu
-set -o pipefail
 
+source ./setup.sh
 REF=$HOME/data/hg19.fa
 
-BFQ=~/data/muc5b/reads/${GROUP}.fq
 
 # create a new index for each job. runs much faster.
 #novoindex -t 4 -c $REF.novo
-<<DONE
-DONE
 
-novoalignCS -s 4 -c 4 -k -d $REF.novo.$LSB_JOBINDEX -f $BFQ -H -F BFASTQ -o SAM -r None \
-     -K data/$GROUP.novo.stats.txt | samtools view -bSF 4 - > data/$GROUP.novo.bam
-
+novoalignCS -c 24 -s 5 -k -d $REF.novo -f $BFQ -H -F BFASTQ -o SAM -r None \
+     -K $OUT/$GROUP.novo.stats.txt | samtools view -bSF 4 - > $OUT/$GROUP.novo.bam
