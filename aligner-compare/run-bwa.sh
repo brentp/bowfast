8b5916fbbf754cd44f6fb6a92df69946bcea5b8e
Brent Pedersen <bpederse@gmail.com>
2011-12-07 13:24:34 -0700

    update bwa to use -k and allow more mismatches

--- aligner-compare/run-bwa.sh
+++ aligner-compare/run-bwa.sh
@@ -10,14 +10,14 @@
 source ./setup.sh
 FASTA=~/data/hg19.fa
 
-bwa aln -q 15 -c -t $THREADS -l 22 -n 3 -O 4 -k 2 $FASTA $WFQ > $OUT/$GROUP.sai
+bwa aln -q 25 -c -t $THREADS -l 22 -n 5 -k 1 $FASTA $WFQ > $OUT/$GROUP.sai
 
 # bwa doesn't trim the quals after trimming the reads...
-bwa samse $FASTA $OUT/$GROUP.sai $WFQ | \
+bwa samse $FASTA $OUT/$GROUP.sai $WFQ -n 1 | \
     awk 'BEGIN{FS=OFS="\t"}
          ($1 ~ /^@/){ print $0} 
          ($1 !~ /^@/){ $11 = substr($11, 0, length($10)); print $0}' | \
-    samtools view -bSF 4 - > $OUT/$GROUP.bwa.unsorted.bam
+    samtools view -bSF 4 - > $OUT/$GROUP.bwa-x.bam
 
-samtools sort -m 3500000000 $OUT/$GROUP.bwa.unsorted.bam $OUT/$GROUP.bwa
-samtools index $OUT/$GROUP.bwa.bam 
+#samtools sort -m 3500000000 $OUT/$GROUP.bwa.unsorted.bam $OUT/$GROUP.bwa-x
+#samtools index $OUT/$GROUP.bwa.bam 
