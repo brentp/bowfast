7081aa3fd61385a2a2a0731d7d7816f5f09753bd
Brent Pedersen <bpederse@gmail.com>
2011-12-20 12:15:53 -0700

    start of stuffs for 5500

---
+++ aligner-compare/scripts/csfasta-from-bfastq.py
@@ -0,0 +1,22 @@
+import sys
+fq = sys.argv[1]
+
+base = fq.split(".bfast.fastq")[0]
+quals = open(base + "_F3_QV.qual", "w")
+seq = open(base + "_F3.csfasta", "w")
+
+for i, line in enumerate(open(fq)):
+
+    mod = i % 4
+    if mod == 0: # name
+        assert line[0] == "@"
+        quals.write(">" + line[1:])
+        seq.write(">" + line[1:])
+    elif mod == 1: # cseq
+        seq.write(line)
+    elif mod == 3:
+        print >>quals, " ".join((str(ord(q) - 33) for q in line.rstrip("\r\n")))
+
+seq.close(); quals.close()
+print >>sys.stderr, "wrote %s, %s" % (quals.name, seq.name)
+
--- aligner-compare/setup.sh
+++ aligner-compare/setup.sh
@@ -3,17 +3,30 @@ GROUP=DNApool_AB
 export LC_ALL=C
 set -e
 
+GROUP=f5500
+OUT=data
+
+<<DONE
+# for 5500 only...
+BFQ=${OUT}/DNA46_2.sub.bfastq
+zcat ~/data/muc5b/2012/reads/muc5b-1-DNA46_2.bfastq.gz  \
+    | head -n $((4777222 * 4 + 1000000)) | tail -n $((4777222 * 4)) \
+    > data/DNA46_2.bfast.fastq
+python scripts/csfasta-from-bfastq.py data/DNA46_2.bfast.fastq
+mv data/DNA46_2_F3_QV.qual data/${GROUP}_F3_QV.qual
+mv data/DNA46_2_F3.csfasta data/${GROUP}_F3.csfasta 
+ln -s $BFQ data/${GROUP}.bfast.fastq
+DONE
+
+BFQ=${OUT}/${GROUP}.bfast.fastq
 THREADS=20
 FASTA=$HOME/data/bfast/hg19All.fa
-GROUP=DNApool_AB
 
 PATH=$PATH:~/src/novoalign/novocraft/:~/src/novoalign/novoalignCS:~/src/bfast-git/scripts/:~/src/bfast-git/
 PATH=$PATH:~/src/bwa:/vol2/home/brentp/src/bowtie/bowtie-0.12.7/:$HOME/src/mosaik-aligner-svn/bin/
 PATH=$PATH:~/src/maq/maq-svn/
 
-OUT=data
 
-BFQ=${OUT}/${GROUP}.fq
 if [ ! -e $BFQ ]; then
     solid2fastq data/${GROUP}_F3.csfasta \
                 data/${GROUP}_F3_QV.qual > $BFQ
